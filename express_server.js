02283cadc5f19ec358edff4c5073ff9cd1456014
Sahil Athia <sahilathia918@gmail.com>
2020-11-10 22:13:07 +0000

    installed and required cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 const app = express();
 const PORT  = 8080;
-const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs")
