05bd2c1f11be21beb3b597ba71f55ec6eee1cecc
Sahil Athia <sahilathia918@gmail.com>
2020-11-10 00:57:34 +0000

    added a redirect to long url based on a short url

--- express_server.js
+++ express_server.js
@@ -6,8 +6,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs")
 
-function generateRandomString() {
-
+let generateRandomString =  () => {
+   return Math.random().toString(36).substring(7);
 };
 
 const urlDatabase = {
@@ -28,13 +28,20 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 app.get('/urls/new', (req, res) => {
   res.render("urls_new")
 })
 
 app.post('/urls', (req, res) => {
   console.log(req.body)
-  res.send("ok")
+  const shortURL = generateRandomString()  
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`)
 })
 
 app.get('/urls/:shortURL', (req, res) => {
