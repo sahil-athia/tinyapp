43eafbfcca48f67daa810003203311be68dcf3a8
Sahil Athia <sahilathia918@gmail.com>
2020-11-12 22:32:43 +0000

    linted code

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
-const generateRandomString =  () => {
+const generateRandomString = () => {
   return Math.random().toString(36).substring(7);
 };
 
@@ -9,36 +9,46 @@ const userLogin = (db, userEmail, userPassword) => {
   for (const user in db) {
     if (db[user].email === userEmail) {
       if (bcrypt.compareSync(userPassword, db[user].password)) {
-        let profile = db[user]
-        return { error: null, profile }
+        let profile = db[user];
+        return { error: null, profile };
       } else {
-        return { error: 'Password or login was incorrect', user: null }
+        return { error: 'Password or login was incorrect', user: null };
       }
     }
   }
-  return { error: 'Password or login was incorrect', user: null }
-}
+  return { error: 'Password or login was incorrect', user: null };
+};
 
 const getUserByEmail = (db, userEmail) => {
   for (const user in db) {
     if (db[user].email === userEmail) {
-      const profile = db[user]
-      return profile
+      const profile = db[user];
+      return profile;
     }
   }
-  return undefined 
-}
+  return undefined;
+};
 
 const urlsForUser = (db, id) => {
-  let matchingUrls = {}
-  if (id){
+  let matchingUrls = {};
+  if (id) {
     for (let urls in db) {
       if (db[urls].userID === id['id']) {
-        matchingUrls[urls] = db[urls]
+        matchingUrls[urls] = db[urls];
       }
     }
-    return matchingUrls
-  }  
-}
+    return matchingUrls;
+  }
+};
 
-module.exports = { getUserByEmail, userLogin, generateRandomString, urlsForUser }
+const idSearch = (matchingUrls, shortURL) => {
+  // this function confirms that a short url belongs to a certain user for error handling
+  for (const key in matchingUrls) {
+    if (key === shortURL) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = { getUserByEmail, userLogin, generateRandomString, urlsForUser, idSearch };
